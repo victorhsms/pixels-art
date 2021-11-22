0fc01460dd9217be9a5b0448bba1f5354c953424
Victor Hugo <victorhsmsantos@gmail.com>
2021-11-22 17:09:35 -0300

    pixels ja podem ser pintados

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const palettes = {
   },
 
   unevenPalettes() {
-    let palette = document.querySelectorAll('color');
+    let palette = document.querySelectorAll('.color');
 
     if (palette[1].style.backgroundColor === palette[2].style.backgroundColor || palette[1].style.backgroundColor === palette[3].style.backgroundColor) {
       document.location.reload(true);
@@ -31,6 +31,9 @@ const palettes = {
       } else {
       paletteColors.style.backgroundColor = palettes.paletteRandomColor(paletteColors.style.backgroundColor);
       }
+      paletteColors.addEventListener('click', function (event) {
+        colorSelected = paletteColors.style.backgroundColor;
+      })
 
       boxesOfPalettes.appendChild(paletteColors);
     };
@@ -39,10 +42,38 @@ const palettes = {
   },
 }
 
-function DOM() {
+const board = {
+  boardGenerator() {
+    let fullFrame = document.querySelector('#pixel-board');
+
+    for (let i = 0; i < 5; i += 1) {
+      let rowBoard = document.createElement('div');
+      rowBoard.className = 'pixel-row';
+      fullFrame.appendChild(rowBoard);
+
+      for (let j = 0; j < 5; j += 1) {
+        let pixels = document.createElement('div');
+        pixels.className = 'pixel';
+        pixels.addEventListener('click', function(event) {
+          event.target.style.backgroundColor = colorSelected;
+        })
+
+        rowBoard.appendChild(pixels);
+      }
+    }
+  }
+}
+
+function initDom() {
+  // Variável principal do programa.
+  var colorSelected = 'rgb(0, 0, 0)';
+
   // Gerar paletas selecionaveis que não sejam brancas e verifica se as cores delas são diferentes
   palettes.paletteGenerator();
 
+  // Gerar quadro em brancos
+  board.boardGenerator();
+
 };
 
-DOM();
+initDom();
